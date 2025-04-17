b0df70604ff2519a549b60cbaecb51efa4c82d5b
an-ivannikov <info@ivannikov.pro>
2025-04-17 22:48:57 +0400

    Updates

---
+++ static/assets/js/chevron-bottom--41k7OlK.js
@@ -0,0 +1,8 @@
+import{a3 as o}from"./Themecontext-Dj1Zc1Om.js";import"./index-8taTtidq.js";import"./context-Db7bLoJ5.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M1.46 4.96a1 1 0 0 1 1.41 0L8 10.09l5.13-5.13a1 1 0 1 1 1.41 1.41l-5.83 5.84a1 1 0 0 1-1.42 0L1.46 6.37a1 1 0 0 1 0-1.41Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{t as chevronBottomSvg};
