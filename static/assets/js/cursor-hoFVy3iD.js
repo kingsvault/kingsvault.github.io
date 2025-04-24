7d5cdfc4f18a03559e72c2419f4a94702a1d9607
an-ivannikov <info@ivannikov.pro>
2025-04-24 13:46:31 +0400

    Updates

--- static/assets/js/cursor-hoFVy3iD.js
+++
@@ -1,3 +0,0 @@
-import{af as o}from"./context-BG1XeK5Q.js";import"./index-BTuJun7W.js";const i=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{i as cursorSvg};
