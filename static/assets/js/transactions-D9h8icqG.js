834271c088f9f9cba88af0cdb65024993fa0c900
an-ivannikov <info@ivannikov.pro>
2025-04-25 17:57:37 +0400

    Updates

---
+++ static/assets/js/transactions-D9h8icqG.js
@@ -0,0 +1,16 @@
+import{x as f,y as m,C as a}from"./context-MRO-Jw1k.js";import{c as p}from"./if-defined-BPGcxobl.js";import"./index-C5SZExx9.js";import"./index-xZ4Es3jI.js";import"./index-CJY4h9do.js";import"./index-nzk_wSqa.js";import"./index-XE8LAoil.js";import"./index-D6NQdkT5.js";import"./index-6m0sKW1c.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var s=o.length-1;s>=0;s--)(l=o[s])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let c=class extends m{render(){return a`
+      <wui-flex flexDirection="column" .padding=${["0","m","m","m"]} gap="s">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};c.styles=d;c=u([p("w3m-transactions-view")],c);export{c as W3mTransactionsView};
