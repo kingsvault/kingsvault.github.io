38510a0dd9cefd673265317f1662ef38c7ba0376
an-ivannikov <info@ivannikov.pro>
2025-04-26 15:42:56 +0400

    Updates

---
+++ static/assets/js/Kings Vault White-BUb-VZj8.js
@@ -0,0 +1,1 @@
+const s="/static/assets/media/Kings%20Vault%20White-Dg_dYWoM.png";export{s as l};
---
+++ static/assets/js/transactions-rMqhZ38j.js
@@ -0,0 +1,16 @@
+import{x as f,y as m,C as a}from"./context-COxjxB3U.js";import{c as p}from"./if-defined-CzuP-3jd.js";import"./index-Dy1L7JIn.js";import"./index-CSdekHak.js";import"./index-C9KZehQa.js";import"./index-gSn8FZtk.js";import"./index-DYwGo8Ox.js";import"./index-CebrCUPw.js";import"./index-Ci6JJGue.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var s=o.length-1;s>=0;s--)(l=o[s])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let c=class extends m{render(){return a`
+      <wui-flex flexDirection="column" .padding=${["0","m","m","m"]} gap="s">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};c.styles=d;c=u([p("w3m-transactions-view")],c);export{c as W3mTransactionsView};
